34d356f50799e606cede8ede17df29e1bd61fd58
KuraoHikari <dewaindra705@gmail.com>
2025-04-01 13:29:26 +0800

    first commit

---
+++ src/lib/create-app.ts
@@ -0,0 +1,23 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
+
+import { pinoLogger } from "@/middlewares/pino-logger";
+
+import type { AppBindings } from "./types";
+
+export function createRouter() {
+  return new OpenAPIHono<AppBindings>(
+    {
+      strict: false,
+    },
+  );
+}
+
+export default function createApp() {
+  const app = createRouter();
+  app.use(pinoLogger());
+
+  app.notFound(notFound);
+  app.onError(onError);
+  return app;
+}
