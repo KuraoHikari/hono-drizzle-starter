0a4b40974074b879820137369f6b48f65ecfb3a1
KuraoHikari <dewaindra705@gmail.com>
2025-04-05 19:49:45 +0800

    chore: finish first reference setup

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,16 +1,16 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError } from "stoker/middlewares";
+import { defaultHook } from "stoker/openapi";
 
 import { pinoLogger } from "@/middlewares/pino-logger";
 
 import type { AppBindings } from "./types";
 
 export function createRouter() {
-  return new OpenAPIHono<AppBindings>(
-    {
-      strict: false,
-    },
-  );
+  return new OpenAPIHono<AppBindings>({
+    strict: false,
+    defaultHook,
+  });
 }
 
 export default function createApp() {
